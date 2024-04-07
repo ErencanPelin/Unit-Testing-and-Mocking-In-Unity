c16ff2c8ab676aacef1808494141fffab6c680bf
Erencan Pelin <88778734+ErencanPelin@users.noreply.github.com>
2024-04-08 08:42:20 +1000

    Update SimpleCharacterController.cs

--- Starter-Code/SimpleCharacterController.cs
+++ Starter-Code/SimpleCharacterController.cs
@@ -7,39 +7,42 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-//very basic character controller using new Unity InputSystem, just so we can move the character with keyboard or gamepad or similar device
-[RequireComponent(typeof(PlayerInput))]
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(CapsuleCollider2D))]
-public class SimpleCharacterController : MonoBehaviour
+namespace TestingWorkshop.Runtime
 {
-  //fields
-  [SerializeField] private float moveSpeed;
-
-  //component refs
-  private PlayerInput playerInput;
-  private Transform _transform;
-  private Rigidbody2D rigidbody;
-
-  private Vector2 movement;
+  //very basic character controller using new Unity InputSystem, just so we can move the character with keyboard or gamepad or similar device
+  [RequireComponent(typeof(PlayerInput))]
+  [RequireComponent(typeof(Rigidbody2D))]
+  [RequireComponent(typeof(CapsuleCollider2D))]
+  public class SimpleCharacterController : MonoBehaviour
+  {
+    //fields
+    [SerializeField] private float moveSpeed;
   
-  private void Awake()
-  {
-    //get refs
-    _transform = transform;
-    playerInput = GetComponent<PlayerInput>();
-    rigidbody = GetComponent<Rigidbody>();
-  }
-
-  private void Update()
-  {
-    //get input
-    movement = playerInput.actions["Move"].readValue<Vector2>();
-  }
+    //component refs
+    private PlayerInput playerInput;
+    private Transform _transform;
+    private Rigidbody2D rigidbody;
   
-  private void FixedUpdate()
-  {
-    //apply movement
-    rigidbody.velocity = movement * moveSpeed;
+    private Vector2 movement;
+    
+    private void Awake()
+    {
+      //get refs
+      _transform = transform;
+      playerInput = GetComponent<PlayerInput>();
+      rigidbody = GetComponent<Rigidbody>();
+    }
+  
+    private void Update()
+    {
+      //get input
+      movement = playerInput.actions["Move"].readValue<Vector2>();
+    }
+    
+    private void FixedUpdate()
+    {
+      //apply movement
+      rigidbody.velocity = movement * moveSpeed;
+    }
   }
 }
